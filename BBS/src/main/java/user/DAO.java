9ce871b81b20268719d5043bc114a181dd364ebf
hihijoo <shrkfl4555@gmail.com>
2022-07-25 01:02:52 +0900

    게시판 만들기 연습 1일차 - mysql 말고 oracle로 연동하기 성공

---
+++ BBS/src/main/java/user/DAO.java
@@ -0,0 +1,79 @@
+package user;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DAO {
+
+	//ORACLE DB 정보
+		private String jdbcDriver ="oracle.jdbc.driver.OracleDriver";
+		private String oracleUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+		private String connectedId ="hr";
+		private String connectedPwd ="hr";
+		
+		//공통으로 사용되는 필드 - 
+		protected Connection conn;
+		protected Statement stmt;
+		protected PreparedStatement pstmt;
+		protected ResultSet rs;
+		
+		//한번만 dbconfig를 실행시키고자 한다면 빼내야함
+		public DAO() {
+//			dbConfig();
+		}
+		
+		//db에 접속하는 메소드
+		public void connect() {
+			
+			try {
+				Class.forName(jdbcDriver);
+				conn = DriverManager.getConnection(oracleUrl,connectedId, connectedPwd);
+				
+			}catch(ClassNotFoundException e) {
+				System.out.println("jdbc driver 로딩 실패");
+			}catch(SQLException e) {
+				System.out.println("DB 연결 실패");
+			}
+			
+		}
+		
+		//db 정보를 가져오는 메소드
+
+		private void dbConfig() { // 파일에 저장된것을 가져옴, 파일값만 바꾸면 되니까 편리함
+			String resource = "config/db.properties";
+			Properties properties = new Properties();
+			
+			try {                //class 정보를 가져오는것, 지금실행되는 클래스의 정보, 접근위치 넘겨 자원찾기, 경로를 가져옴
+				String filePath = ClassLoader.getSystemClassLoader().getResource(resource).getPath();
+				properties.load(new FileInputStream(filePath)); //파일 읽어들이는 방식
+				
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			//map에 key과 value 의 형태
+			jdbcDriver = properties.getProperty("driver");
+			oracleUrl = properties.getProperty("url");
+			connectedId = properties.getProperty("id");
+			connectedPwd = properties.getProperty("password");
+		}
+
+		
+		//db 접속을 해제하는 메소드
+		public void disconnect() {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(stmt != null) conn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+}
---
+++ BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,50 @@
+package user;
+
+
+public class UserDAO extends DAO{
+
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USERS WHERE userID =?";
+		connect();
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				} else
+					return 0;
+			}
+			return -1; // 아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return -2; // 데이터베이스 오류
+	}
+	
+	public int join(User user) {
+		String SQL ="INSERT INTO USERS VALUES (?,?,?,?,?)";
+		connect();
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+		return pstmt.executeUpdate();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return -1; //데이터베이스 오류
+	}
+
+}
